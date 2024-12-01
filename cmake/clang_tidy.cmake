d162a59e09534d488a5b8ad424b8985f149b8563
PozhiloyPumba <ivanov.in@phystech.edu>
2024-12-01 06:00:19 +0300

    added ci and clang-tidy

--- cmake/clang_tidy.cmake
+++ cmake/clang_tidy.cmake
@@ -8,17 +8,19 @@ macro(add_recursive retVal dir ext1 excl)
     endforeach()
 endmacro(add_recursive)
 
-find_program(CLANG_FORMAT_EXECUTABLE NAMES clang-format)
+find_program(CLANG_TIDY_EXECUTABLE NAMES clang-tidy)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-function(add_format_target)
+function(add_tidy_target)
     add_recursive(ALL_SOURCES_FILES_CXX "." "*.[ch]pp" "/build/")
     add_recursive(ALL_SOURCES_FILES_C "." "*.[ch]" "/build/")
 
-    add_custom_target(run-clang-format
-        COMMAND ${CLANG_FORMAT_EXECUTABLE}
-        -i
-        -style=file
-        --verbose
+    add_custom_target(run-clang-tidy
+        COMMAND ${CLANG_TIDY_EXECUTABLE}
+        -format-style=file
+        -header-filter=.*
+        -p ${CMAKE_BINARY_DIR}
+        --fix-errors
         ${ALL_SOURCES_FILES_C}
         ${ALL_SOURCES_FILES_CXX}
     )
